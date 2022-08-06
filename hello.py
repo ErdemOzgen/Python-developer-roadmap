217d745f06d978a95083a2a88951030488326412
Erdem Özgen <m.erdemozgen@gmail.com>
2022-08-06 14:25:21 +0300

    Add python welcome code

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Says Welcome to Python Developer Roadmap Users
+def welcome():
+    """ Function print outs welcome to python developer roadmap """
+    print("Welcome to Python Developer Roadmap")
+
+def goodLuck():
+    """ Function print outs finishing sentence """
+    print("Good luck, Have fun and DO NOT FORGET PRs ARE WELCOME")
+
+
+if __name__ == "__main__":
+    welcome()
+    goodLuck()
